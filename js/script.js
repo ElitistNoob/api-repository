e13edd1d2fa1a9f526f46a86736082bd891b92ad
Kevin Hernot <89539295+ElitistNoob@users.noreply.github.com>
2022-08-21 12:58:01 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import { renderAPI, renderCategories } from './html.js'
 import { searched, filterFunc } from './utils.js'
 
 // const searchBar = document.getElementById('search-bar').value = ""
-const modeSwitch = document.getElementById('mode')
+const clickHandler = document.getElementById('mode')
 const bodyClassList = document.body.classList
 let currentMode = localStorage.getItem('mode')
 
@@ -28,26 +28,32 @@ getAPICategories().then(categories => {
 
 
 // SAVES THEME PREFERENCE TO LOCAL STORAGE IF  NO PREFERENCE WILL DEFAULT TO DARK
+clickHandler.addEventListener('click', () => {
+  resetTheme()
+})
+
 if (currentMode === null || currentMode === 'dark') {
   bodyClassList.add('dark')
 } else if (currentMode === 'light') {
   bodyClassList.add('light')
 } 
 
-modeSwitch.addEventListener('click', () => {
-  resetTheme()
-})
+const classSwitch = (remove, add) => {
+  bodyClassList.remove(remove)
+  bodyClassList.add(add)
+}
+
+const storageHandler = (remove, add) => {
+  localStorage.removeItem('mode', remove)
+  localStorage.setItem('mode', add)
+}
 
 const resetTheme = () => {
-  if (modeSwitch.checked) {
-    bodyClassList.remove('dark')
-    bodyClassList.add('light')
-    localStorage.removeItem('mode', 'dark')
-    localStorage.setItem('mode', 'light')
+  if (clickHandler.checked) {
+    classSwitch('dark', 'light')
+    storageHandler('dark', 'light')
   } else {
-    bodyClassList.remove('light')
-    bodyClassList.add('dark')
-    localStorage.removeItem('mode', 'light')
-    localStorage.setItem('mode', 'dark')
+    classSwitch('light', 'dark')
+    storageHandler('light', 'dark')
   }
 }
